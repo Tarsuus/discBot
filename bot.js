c45d8550beaf52c2aa77ac6e108ebb5a4683d886
Tarsuus <Tarsuus@gmail.com>
2018-07-21 13:46:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,20 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong');
+    	message.reply('C\'est ...la mer noire');
   	}
 });
 
-client.on('UserConnection', userConnection => {
-   console.log('User connected, userId:['+userConnection.id+'], name:['+userConnection.name+'],revoked:['+userConnection.revoked+'],type:['+userConnection.type+'],user:['+userConnection.user+']');
+client.on('presenceUpdate', (oldMember,newMember) => {
+    console.log(' oldMumber joinedAt '+ oldMember.joinedAt);   
+    console.log(' newMember joinedAt '+ newMember.joinedAt);   
+    console.log(' oldMumber joinedTimestamp '+ oldMember.joinedTimestamp);   
+    console.log(' newMember joinedTimestamp'+ newMember.joinedTimestamp);   
+});
+
+client.on('guildMemberAdd', member => {
+    console.log('new member joinedAt '+ member.joinedAt);
+    console.log('new member joinedTimestamp '+ member.joinedTimestamp);
 });
 
 // THIS  MUST  BE  THIS  WAY
