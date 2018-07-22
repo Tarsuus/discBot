2cf396dbdd7af5080d86d61ebfef68cf81c1b521
Tarsuus <Tarsuus@gmail.com>
2018-07-22 07:49:46 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   console.log("works");
+    console.log("message "+message);
+    console.log("args "+args);
 }
 
 module.exports.help = {
