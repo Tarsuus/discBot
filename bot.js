6915a282b19ba331ecd458fd921a2dde7e8370b7
Tarsuus <Tarsuus@gmail.com>
2018-07-22 07:30:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
+bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
 
     if(err) console.log(err);
     let jsfile = files.filter(f => f.split(".").pop() === "js")
     if(jsfile.length <= 0) {
-     console.log("Commandes introuvable");
+     console.log('Commandes introuvable');
      return;
     }
-})
+
+    jsfile.forEach((f, i) => {
+        let props = require('./commands/${f}');
+        console.log('${f} chargé !');
+        bot.commands.set(props.help.name, props);
+    });
+    
+        
+    console.log(bot.commands.size);
+});
 
 
 client.on('ready', () => {
