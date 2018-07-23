aa48624535d42be0ec17384f140d4ba81b5c77c8
Tarsuus <Tarsuus@gmail.com>
2018-07-23 20:26:53 +0200

    Update chuck.js

--- commands/chuck.js
+++ commands/chuck.js
@@ -1,6 +1,24 @@
 const Discord = require("discord.js");
+const https = require('https');
+
 
 module.exports.run = async (bot, message, args) => {
+  
+  https.get('https://www.chucknorrisfacts.fr/api/get?data=nb:1', (resp) => {
+    let data = '';
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      console.log(JSON.parse(data).explanation);
+    });
+ 
+  }).on("error", (err) => {
+      console.log("Error: " + err.message);
+     });
+  
   console.log("works");
     console.log("message "+message);
     console.log("args "+args);
