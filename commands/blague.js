4bb4f19cc0298c354986fa9efcc98c9017c489ba
Tarsuus <Tarsuus@gmail.com>
2018-07-23 20:18:29 +0200

    Delete blague.js

--- commands/blague.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-  console.log("Blague commande executé");
-}
-
-module.exports.help = {
-  name: "blague"
-}
