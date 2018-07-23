d76c7a8c23cb946ba463ce6db5fb9aea8fb9a0ea
Tarsuus <Tarsuus@gmail.com>
2018-07-23 20:30:06 +0200

    Update chuck.js

--- commands/chuck.js
+++ commands/chuck.js
@@ -4,24 +4,26 @@ const https = require('https');
 
 module.exports.run = async (bot, message, args) => {
   
+  console.log("Chuck commande");
+  
   https.get('https://www.chucknorrisfacts.fr/api/get?data=nb:1', (resp) => {
     let data = '';
     // A chunk of data has been recieved.
     resp.on('data', (chunk) => {
       data += chunk;
+      console.log("test ["+data+"]");
     });
 
     resp.on('end', () => {
+        console.log("test1");
       console.log(JSON.parse(data).explanation);
     });
  
   }).on("error", (err) => {
+      console.log("test2");
       console.log("Error: " + err.message);
      });
   
-  console.log("works");
-    console.log("message "+message);
-    console.log("args "+args);
 }
 
 module.exports.help = {
