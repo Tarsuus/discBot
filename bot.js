187b06fd3da80b9a1118efc66876c0e5cffa2716
Tarsuus <Tarsuus@gmail.com>
2020-02-14 17:46:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ bot.on("ready", () => {
 bot.on("message", message => {
     if (message.content === "!news") {
     	 if(message.channel.name ==="📰-socks-breaking-news2" ){
-             message.channel.send(":musical_note: :musical_note:\n @everyone Socks breaking news interrompt votre émission pour vous rappeler que notre grand gourou est parti dans des contrés encore inexploré pour répandre la bonne parole une fois de plus ! \n Il ne pourra donc pas assurer ses séances farineuses du samedi soir et dimanche matin. Il reviendra de son périple possiblement pour la séance du dimanche soir ! \n Nous lui souhaitons tous bonne chance dans son aventure, c'était socks breaking news, pour les chaussettes et par les chaussettes !\n :musical_note: :musical_note:",{disableEveryone: false})
+             message.channel.send(":musical_note: :musical_note:\n @everyone Socks breaking news interrompt votre émission pour vous rappeler que notre grand gourou est parti dans des contrés encore inexploré pour répandre la bonne parole ! \n Il ne pourra donc pas assurer ses séances farineuses du samedi soir et dimanche matin. Il reviendra de son périple possiblement pour la séance du dimanche soir ! \n Nous lui souhaitons tous bonne chance dans son aventure, c'était socks breaking news, pour les chaussettes et par les chaussettes !\n :musical_note: :musical_note:",{disableEveryone: false})
          }
     }
     let messageArray = message.content.split(" ");
@@ -37,13 +37,6 @@ bot.on("message", message => {
     
     let commandfile = bot.commands.get(cmd.slice(1));
     if(commandfile) commandfile.run(bot,message,args);
-});
-
-bot.on('presenceUpdate', (oldMember, newMember) => {
-  console.log('old:'+ oldMember.user.displayName);
-  console.log(oldMember.user.joinedAt);
-  console.log('new:'+ newMember.user.displayName);
-  console.log(newMember.user.joinedAt);
 });
 
 bot.on('guildMemberAdd', member => {
