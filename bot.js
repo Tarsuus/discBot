743c6e2a78324d979d7c1f3006016bf02f242c2e
Tarsuus <Tarsuus@gmail.com>
2020-02-14 17:35:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,9 @@ bot.on("ready", () => {
 
 bot.on("message", message => {
     if (message.content === "blabla mister freeman") {
-    	message.reply(":musical_note: :musical_note:\n @everyone Socks breaking news interrompt votre émission pour vous rappeler que notre grand gourou est parti dans des contrés encore inexploré pour répandre la bonne parole une fois de plus ! \n Il ne pourra donc pas assurer ses séances farineuses du samedi soir et dimanche matin. Il reviendra de son périple possiblement pour la séance du dimanche soir ! \n Nous lui souhaitons tous bonne chance dans son aventure, c'était socks breaking news, pour les chaussettes et par les chaussettes !\n :musical_note: :musical_note:",{reply: null})
+    	//message.reply(":musical_note: :musical_note:\n @everyone Socks breaking news interrompt votre émission pour vous rappeler que notre grand gourou est parti dans des contrés encore inexploré pour répandre la bonne parole une fois de plus ! \n Il ne pourra donc pas assurer ses séances farineuses du samedi soir et dimanche matin. Il reviendra de son périple possiblement pour la séance du dimanche soir ! \n Nous lui souhaitons tous bonne chance dans son aventure, c'était socks breaking news, pour les chaussettes et par les chaussettes !\n :musical_note: :musical_note:")
+        message.channel.send("test");
+        //message.channel.send(":musical_note: :musical_note:\n @everyone Socks breaking news interrompt votre émission pour vous rappeler que notre grand gourou est parti dans des contrés encore inexploré pour répandre la bonne parole une fois de plus ! \n Il ne pourra donc pas assurer ses séances farineuses du samedi soir et dimanche matin. Il reviendra de son périple possiblement pour la séance du dimanche soir ! \n Nous lui souhaitons tous bonne chance dans son aventure, c'était socks breaking news, pour les chaussettes et par les chaussettes !\n :musical_note: :musical_note:")
     }
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
