51a1edccd01abe90717a17fb2496a67b129112e9
Tarsuus <Tarsuus@gmail.com>
2020-02-14 17:51:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ bot.on("ready", () => {
 
 bot.on("message", message => {
     if (message.content === "!news") {
-    	 if(message.channel.name ==="📰-socks-breaking-news" ){
+    	 if(message.channel.name ==="📰-socks-breaking-news" || message.channel.name ==="testBot"){
              message.channel.send(":musical_note: :musical_note:\n @everyone Socks breaking news interrompt votre émission pour vous rappeler que notre grand gourou est parti dans des contrés encore inexploré pour répandre la bonne parole ! \n Il ne pourra donc pas assurer ses séances farineuses du samedi soir et dimanche matin. Il reviendra de son périple possiblement pour la séance du dimanche soir ! \n Nous lui souhaitons tous bonne chance dans son aventure. C'était Socks breaking news, pour les chaussettes et par les chaussettes !\n :musical_note: :musical_note:",{disableEveryone: false})
          }
     }
